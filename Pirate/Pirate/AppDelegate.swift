73402ba462cc55395f8147f05b596834b43d95f1
hyperorchid <hyperorchid@outlook.com>
2019-09-29 12:38:31 +0800

    init

---
+++ Pirate/Pirate/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Pirate
+//
+//  Created by hyperorchid on 2019/9/29.
+//  Copyright © 2019年 hyperorchid. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+        @IBOutlet weak var window: NSWindow!
+
+
+        func applicationDidFinishLaunching(_ aNotification: Notification) {
+                // Insert code here to initialize your application
+        }
+
+        func applicationWillTerminate(_ aNotification: Notification) {
+                // Insert code here to tear down your application
+        }
+
+
+}
+
