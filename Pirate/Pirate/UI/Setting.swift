71db8ad08405f8b464d8638f2e1630976b5b722d
hyperorchid <hyperorchid@outlook.com>
2019-11-08 18:49:18 +0800

    add setting ui

---
+++ Pirate/Pirate/UI/Setting.swift
@@ -0,0 +1,19 @@
+//
+//  Setting.swift
+//  Pirate
+//
+//  Created by hyperorchid on 2019/11/8.
+//  Copyright © 2019 hyperorchid. All rights reserved.
+//
+
+import Cocoa
+
+class Setting: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
